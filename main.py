cae11251defd47e90bf9226e2f4cc2b5792a13b1
Jeremy Martin <83788197+JerRMartin@users.noreply.github.com>
2025-09-19 16:51:39 -0400

    Added TODOs

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ from vidstab import VidStab
 stable_output_folder = "stabilized_videos"
 videos_input_folder = "videos"
 
+#TODO: Do this for each video we want to process, keep in mind we have 5, we only need 5
+
 extract_frames(
     f"{videos_input_folder}/Couch film of John F. Kennedy assassination.mp4", 
     "extracted_frames/couch_film/original", 
-    10, 
-    11
+    10, #TODO: decide our actual start time
+    11 #TODO: Decide our actual end time
     )
 
 for frame in os.listdir("extracted_frames/couch_film/original"):
@@ -20,12 +22,13 @@ for frame in os.listdir("extracted_frames/couch_film/original"):
     #                 f"extracted_frames/couch_film/original/{frame}", 
     #                 f"extracted_frames/couch_film/high_pass/{frame}")
 
-    # # Histogram equalize each frame
-    # hist_eq_frame(f"extracted_frames/couch_film/original/{frame}", 
-    #               f"extracted_frames/couch_film/equalized/{frame}")
+    # Histogram equalize each frame
+    hist_eq_frame(f"extracted_frames/couch_film/original/{frame}", #TODO: change this to the output of high-pass filter
+                  f"extracted_frames/couch_film/equalized/{frame}")
     
 
 
+#TODO: Recombine the frames into a video to pass to the stabilizer
 
 
 # # Stabilize video
