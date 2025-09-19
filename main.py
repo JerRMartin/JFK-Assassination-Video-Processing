9cc12a12c362bd08dd7d38106e2f8c7b51076540
Jeremy Martin <83788197+JerRMartin@users.noreply.github.com>
2025-09-19 12:31:34 -0400

    create a utils file, and add helper functions

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-import cv2
-import numpy as np
+import os
+from utils.video_helpers import extract_frames  
 
+extract_frames("Couch film of John F. Kennedy assassination.mp4", "couch_film", 10, 20)
+
+for frame in os.listdir("couch_film"):
+    # TODO: process each frame
+    pass
